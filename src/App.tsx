6d11c0096d5a4ee3f1e6045a2ed704a75957d1a2
ahmad4948 <ahmadshamekh99@gmail.com>
2025-09-29 02:58:34 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <p className='text-green-500 text-6xl'>hi</p>
+    </>
+  )
+}
+
+export default App
