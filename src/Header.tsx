8f158bcaa22ce35f3b616f450b277e65e2698715
ahmad4948 <ahmadshamekh99@gmail.com>
2025-10-05 23:19:54 +0300

    Added Bitcoin picture and modified the header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,19 +1,20 @@
 function Header(){
 return(
-   <nav className="bg-[#0d023c] h-22 flex">
-      <h1 className="text-white text-2xl my-auto ml-12">Cryptoplace</h1>
+   <nav className="bg-[#0d023c] h-22 flex border-b-3 border-gray-600">
+      <img src="bitcoin.png" className="w-24 h-auto ml-6"/>
+      <h1 className="text-white text-2xl my-auto">Cryptoplace</h1>
       <ul className="text-white flex justify-around w-120 mx-auto my-auto">
-         <li className="inline-block"> <a href="#">Home</a></li>
-         <li className="inline-block"> <a href="#">Features</a></li>
-         <li className="inline-block"> <a href="#">Pricing</a></li>
-         <li className="inline-block"> <a href="#">Blogs</a></li>
+         <li className="inline-block cursor-pointer"> <a href="#">Home</a></li>
+         <li className="inline-block cursor-pointer"> <a href="#">Features</a></li>
+         <li className="inline-block cursor-pointer"> <a href="#">Pricing</a></li>
+         <li className="inline-block cursor-pointer"> <a href="#">Blogs</a></li>
       </ul>
-      <select className="text-white mr-12 bg-[#0d023c]">
+      <select className="cursor-pointer text-white mr-12 bg-[#0d023c] h-8 my-auto">
          <option className="">USD</option>
          <option>EUR</option>
          <option>EGP</option>
       </select>
-      <button className="text-black rounded bg-white mr-24 h-10 my-auto w-16">Sign Up</button>
+      <button className="text-black rounded bg-white mr-24 h-10 my-auto w-16 cursor-pointer">Sign Up</button>
    </nav>
 )
 }
