a83c8ff74b2d6ad1767207bd34b302fc10ff324b
ahmad4948 <ahmadshamekh99@gmail.com>
2025-10-30 02:46:49 +0300

    Fetched coins from api

--- src/Main2.tsx
+++ src/Main2.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import { useEffect } from "react";
 function Main2(){
+   const [coin,setcoins] = useState([]);
+   useEffect(()=>{
+      fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+       .then((res)=>{
+       return res.json()
+       }
+      )
+       .then((data)=>{ 
+         console.log(data);
+         setcoins(data);
+       } )
+       .catch((err)=>console.log("error:" , err));
+   },[]);
+
 return(
 <main className="bg-[#0d023c] text-white h-165 text-center">
    <h1 className="text-6xl mt-24"> 
@@ -10,7 +26,7 @@ return(
 
       <div className="flex justify-center mt-12 items-center">
       <input placeholder="Search crypto.." className="bg-white w-120 h-14 rounded
-       placeholder-gray-600 p-4" ></input>
+       placeholder-gray-600 p-4 text-black" ></input>
        <button className="bg-[#7b3ff3] h-10 p-2 rounded -ml-24 cursor-pointer">Search</button>
        </div>
 </main>
